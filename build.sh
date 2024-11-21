c5520279b01848ee370dee2933a1108e5834cc7a
Yinghui He <67273897+heyinghui22@users.noreply.github.com>
2024-11-21 22:36:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 mkdir -p $PREFIX/bin
-cp -rf $SRC_DIR/miTag_analysis-command $PREFIX/bin
-cp -rf $SRC_DIR/miTag_analysis-scripts $PREFIX/bin
-cp $SRC_DIR/miTag_analysis.py $PREFIX/bin
-
-
-
+cp -rf $SRC_DIR/mitag_analysis-command $PREFIX/bin
+cp -rf $SRC_DIR/mitag_analysis-scripts $PREFIX/bin
+cp $SRC_DIR/mitag_analysis.py $PREFIX/bin
